a06d46cc825ea95873fced342c7cd492cde8d954
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-22 01:39:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,11 +15,11 @@ class Triangle
       raise TriangleError
     end
 
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
+      if @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
         :isosceles
-      else (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+      else
         :scalene
       end
     end
