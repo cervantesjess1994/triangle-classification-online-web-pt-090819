fd4141cd8214104f0b9a6e6c2762727fa1b5a6ac
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-10-22 01:27:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initializate(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if (@side1 <= 0) || (@side2) <= 0 || (@side3 <= 0)
+      raise TriangleError
+    elsif (@side1 + @side2 <= 3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
+      riase TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
